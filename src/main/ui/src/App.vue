82c82ad010deefe672da6c33b5c8cf6c182a3396
Makc <makc.pst@gmail.com>
2021-02-22 19:40:15 +0300

    Переменные стилей

--- src/main/ui/src/App.vue
+++ src/main/ui/src/App.vue
@@ -15,13 +15,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+//Цвета
+$la-color: hsla(233, 20%, 83%, 1); //бэкграунд
+$btn-color: hsla(233, 66%, 91%, 1); //кнопка
+$input-color: hsla(234, 67%, 92% ,1); //поле ввода
+$text-color: hsla(233,7%,50%,1); //текст
+$bg-card-color: hsla(51, 7%, 92%, 1); //фон карточки
+$light-el-bg-color: hsla(233, 66%, 91%, 1); //фон верхнего элемента
+
+$bxsh-down: 9px 9px 16px hsla(228, 48%, 93%, 1); // тень нижняя
+$bxsh-up: -9px -9px 16px hsla(228, 15%, 70%, 1); // тень верхняя
+
+$bdrs: 7px; //скругление границ
+$border: 1px solid hsla(0, 0%, 100%, 0.4); //границы
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Rambla, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $text-color;
+  margin-top: 0;
+  background-color: $la-color;
+  min-height: 100vh;
 }
 </style>
