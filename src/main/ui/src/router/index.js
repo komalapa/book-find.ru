4f3cf2e3be0a83d76223e7968388aedc31f064de
komalapa <komalapa@gmail.com>
2021-03-04 23:06:07 +0400

    auth works

--- src/main/ui/src/router/index.js
+++ src/main/ui/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from "../store"
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -11,6 +12,44 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: () => import('../components/Home')
-    }
+    },
+    {
+      path: '/temphome',
+      name: 'tempHome',
+      component: () => import('../components/temphome')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../components/Login')
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: () => import('../components/Registration')
+    },
+    {
+      path: '/guard', //Защищенные страницы пример
+      name: 'guard',
+      component:  () => import('../components/secure'),
+      meta: { 
+        requiresAuth: true
+      }
+    },
+
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
+})
+
+export default router
