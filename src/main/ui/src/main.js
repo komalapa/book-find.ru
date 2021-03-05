44c43320852db7745579db032c4dcc9af0966072
komalapa <komalapa@gmail.com>
2021-03-05 10:14:50 +0400

    module in store

--- src/main/ui/src/main.js
+++ src/main/ui/src/main.js
@@ -4,9 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import Axios from 'axios'
 
-Vue.prototype.$http = Axios;
 
 Vue.config.productionTip = false
 
